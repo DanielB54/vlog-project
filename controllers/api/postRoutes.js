962789215031f62d4746c603acb8f42168466742
aaronserrands <danielb80136@gmail.com>
2022-09-10 13:36:00 -0700

    working on getting cloudinary running.

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+
+router.post('/', async (req, res) => {
+  try {
+    const postData = req.body;
+    // 200 status code means the request is successful
+    res.status(200).json(postData);
+  } catch (err) {
+    // 400 status code means the server could not understand the request
+    res.status(400).json(err);
+  }
+});
+
+router.get('/newPost', async (req, res) => {
+  try {
+    res.render('index', {
+        layout: 
+    })
+} catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
