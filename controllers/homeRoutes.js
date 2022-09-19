8e9776bb19b0b72bc8eaad183c1c383d61169b4e
ZaneMJohnson <johnsonzanem@gmail.com>
2022-09-18 18:14:18 -0700

    fixing this damn code

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,18 +3,21 @@ const { Vidpost } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/homepage', withAuth, async (req, res) => {
-  //   const vidPostData = await Vidpost.findAll();
-  //   const videos = vidPostData.map((userVideo) => {
-  //     return userVideo.toJSON();
-  //   });
-  //   res.render('homepage', {
-  //     videos
-  //   });
-  res.render('signup');
+  const vidPostData = await Vidpost.findAll();
+  const videos = vidPostData.map((userVideo) => {
+    return userVideo.toJSON();
+  });
+  res.render('homepage', {
+    videos,
+  });
 });
 
 router.get('/login', (req, res) => {
   res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  res.render('signup');
 });
 
 module.exports = router;
